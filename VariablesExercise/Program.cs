a4966afcae1d99efdafa42b5c34babf1e3717ae6
kmadison382 <k.madison.382@gmail.com>
2024-06-05 20:42:12 -0500

    a short blurb about tv shows

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string tvShow = "Star Trek";
+            int years = 6;
+            char grade = 'A';
+            bool watchAll = false;
+            double watchPercent = 64.71;
+            decimal watchHours = 591.942m;
+            Console.WriteLine($"A TV show I love is {tvShow}. I first watched it {years} years ago. Have I watched all the content? {watchAll}. I have watched {watchPercent}% of the series, or {watchHours} hours.");
+            Console.WriteLine($"As a former teacher, I give {tvShow} a solid {grade}!");
         }
     }
 }
